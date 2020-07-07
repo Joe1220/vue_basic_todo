0946fd3694fb631d3c6ea38c184f1ee566e009aa
unknown <joe1220@daum.net>
2020-07-07 18:10:21 +0900

    [add: component] - [add] add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input type="text" class="w-100 p-2" placeholder="Type todo" />
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
     <div>
       <hr />
-      <input type="checkbox" />
-      <span class="ml-3">Buy a Car</span>
+      <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Todo from "@/components/Todo.vue";
+export default {
+  components: {
+    Todo
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = "";
+    }
+  },
+  data() {
+    return {
+      todoText: "",
+      todos: [
+        { id: 1, text: "buy a car", checked: true },
+        { id: 2, text: "buy a car", checked: false }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
