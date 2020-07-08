3614543d2d5b2155a3afe6c9a4aede8a5f3ba7c1
unknown <joe1220@daum.net>
2020-07-08 21:59:01 +0900

    [add: components] - [add] Todo 관련 components 분할

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,28 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input
-      v-model="todoText"
-      type="text"
-      class="w-100 p-2"
-      placeholder="Type todo"
-      @keyup.enter="addTodo"
-    />
+    <CompletedTodo :todos="todos" />
+    <AddTodo @add-todo="addTodo" />
     <div>
       <hr />
-      <Todo
-        v-for="todo in todos"
-        :key="todo.id"
-        :todo="todo"
-        @toggle-checkbox="toggleCheckbox"
-        @click-delete="clickDelete"
-      />
+      <TodoList :todos="todos" @toggle-checkbox="toggleCheckbox" @click-delete="clickDelete" />
     </div>
   </div>
 </template>
 
 <script>
-import Todo from "@/components/Todo.vue";
+import TodoList from "@/components/TodoList.vue";
+import AddTodo from "@/components/AddTodo.vue";
+import CompletedTodo from "@/components/CompletedTodo.vue";
 export default {
-  components: {
-    Todo
-  },
+  components: { TodoList, AddTodo, CompletedTodo },
   methods: {
-    addTodo(e) {
+    addTodo(value) {
       this.todos.push({
         id: Math.random(),
-        text: e.target.value,
+        text: value,
         checked: false
       });
-      this.todoText = "";
     },
     toggleCheckbox({ id, checked }) {
       const index = this.todos.findIndex(todo => {
@@ -43,15 +31,11 @@ export default {
       this.todos[index].checked = checked;
     },
     clickDelete(id) {
-      const index = this.todos.findIndex(todo => {
-        return todo.id === id;
-      });
-      this.todos.splice(index, 1);
+      this.todos = this.todos.filter(todo => todo.id !== id);
     }
   },
   data() {
     return {
-      todoText: "",
       todos: [
         { id: 1, text: "buy a car", checked: false },
         { id: 2, text: "buy a car", checked: false }
---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Todo
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="clickDelete"
+    />
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo.vue";
+export default {
+  comments: { Todo },
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Todo
+  },
+  methods: {
+    toggleCheckbox(value) {
+      this.$emit("toggle-checkbox", value);
+    },
+    clickDelete(todoId) {
+      this.$emit("click-delete", todoId);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
